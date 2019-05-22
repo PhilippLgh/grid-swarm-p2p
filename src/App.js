bb106e9d6257ee7cbb279d942a3f9ec549157218
philipplgh <philipplgh@gmail.com>
2019-05-22 18:56:22 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Dropzone from 'react-dropzone'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    files: [],
+    ready: false
+  }
+  componentDidMount = async () => {
+    const swarm = await window.grid.getClient('swarm')
+    const state = swarm.getState()
+    if(state==='STARTED'){
+      this.setState({
+        ready: true
+      })
+      return
+    }
+    console.log('swarm state', swarm.getState())
+    swarm.on('started', async () => {
+      // use RPC methods here
+      this.setState({
+        ready: true
+      })
+
+    })
+  }
+  onDrop = async acceptedFiles => {
+    const { files } = this.state
+
+    let uploads = acceptedFiles.map(f => {
+      return axios.post('http://localhost:8500/bzz:/', f)
+      .then(result => {
+        return {
+          name: f.name,
+          size: f.size,
+          swarmHash: result.data
+        }
+      })
+      .catch(err => console.log('error during upload', err))
+    })
+
+    let uploadedFiles = await Promise.all(uploads)
+
+    this.setState({files: [...files, ...uploadedFiles]})
+  }
+  renderFileList(){
+    const { files } = this.state
+    return (
+      files.map(file => (
+        <li key={file.name}>
+          {file.name} - {file.size} bytes - {file.swarmHash}
+        </li>
+      ))
+    )
+  }
+  render(){
+    const { ready } = this.state
+    return (
+      <div className="App">
+        <h1>Swarm P2P Sharing</h1>
+        <h2>State: {ready ? 'ready' : 'waiting for Swarm'}</h2>
+        <Dropzone onDrop={this.onDrop}>
+          {({getRootProps, getInputProps}) => (
+            <section className="dropzone">
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        {this.renderFileList()}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
